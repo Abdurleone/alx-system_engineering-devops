6a985d1542a724d6ce9611d61129eef6dccf5587
Abdurleone <abyyyusuf@gmail.com>
2023-09-29 17:05:26 +0300

    0x05

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -17,11 +17,11 @@
 
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
@@ -32,22 +32,22 @@ int infinite_while(void)
 
 int main(void)
 {
-    pid_t pid;
-    char count = 0;
+	pid_t pid;
+	char count = 0;
 
-    while (count < 5)
-    {
-        pid = fork();
-        if (pid > 0)
-        {
-            printf("Zombie process created, PID: %d\n", pid)
-            sleep(1);
-            count++
-        }
-        else
-            exit(0);
-    }
-    infinite_while();
+	while (count < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			count++
+		}
+		else
+			exit(0);
+	}
+	infinite_while();
 
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
