0639e059cc045663c18bfe778c565d72137b47ad
Abdurleone <abyyyusuf@gmail.com>
2024-01-16 19:06:31 +0300

    0x16

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
@@ -12,7 +13,7 @@ def top_ten(subreddit):
         "limit": 10
     }
     response = requests.get(url, headers=headers, params=params,
-            allow_redirects=False)
+                            allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
