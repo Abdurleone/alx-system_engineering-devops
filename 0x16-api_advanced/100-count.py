01fe14cc74c3c9c7f11a192ee71cb30d39ded26b
Abdurleone <abyyyusuf@gmail.com>
2024-01-16 19:18:59 +0300

    0x16

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -26,12 +26,12 @@ def recurse(subreddit, dictionary, after=None):
     """ Queries the Reddit API """
     u_agent = 'Mozilla/5.0'
     headers = {
-            'User-Agent': u_agent
-            }
+        'User-Agent': u_agent
+    }
 
     params = {
-            'after': after
-            }
+        'after': after
+    }
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     res = requests.get(url,
@@ -51,19 +51,19 @@ def recurse(subreddit, dictionary, after=None):
     recurse(subreddit, dictionary, after=after)
 
 
-    def count_words(subreddit, word_list, dictionary=None):
-        """ Init function """
-        if dictionary is None:
-            dictionary = {}
+def count_words(subreddit, word_list, dictionary=None):
+    """ Init function """
+    if dictionary is None:
+        dictionary = {}
 
-        for word in word_list:
-            word = word.lower()
-            if word not in dictionary:
-                dictionary[word] = 0
+    for word in word_list:
+        word = word.lower()
+        if word not in dictionary:
+            dictionary[word] = 0
 
-        recurse(subreddit, dictionary)
+    recurse(subreddit, dictionary)
 
-        sorted_items = sorted(dictionary.items(), keys=lambda kv: (-kv[1], kv[0]))
-        for item in sorted_items:
-            if item[1] > 0:
-                print("{}: {}".format(item[0], item[1]))
+    sorted_items = sorted(dictionary.items(), key=lambda kv: (-kv[1], kv[0]))
+    for item in sorted_items:
+        if item[1] > 0:
+            print("{}: {}".format(item[0], item[1]))
