414273c81c5a0aa1d36df496004bc7e9c0aeebf5
Abdurleone <abyyyusuf@gmail.com>
2024-01-17 19:25:42 +0300

    0x17

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,11 @@
 
 $file_to_edit = '/var/www/html/wp-settings.php'
 
+file { 'wp_settings_file':
+    path    => $file_to_edit,
+    ensure  => file,
+}
+
 #replace line containing "phpp" with "php"
 
 exec { 'replace_line':
