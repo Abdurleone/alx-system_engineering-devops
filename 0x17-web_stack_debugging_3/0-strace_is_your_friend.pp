769df1d8bb440643521da94ae881691fec1f2402
Abdurleone <abyyyusuf@gmail.com>
2024-01-17 19:16:28 +0300

    0x17

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,7 +4,7 @@ $file_to_edit = '/var/www/html/wp-settings.php'
 
 #replace line containing "phpp" with "php"
 
-exec {
+exec { 'replace_line':
     command => "sed -i 's/phpp/php/g' ${file_to_edit}",
     path    => ['/bin','/usr/bin'],
 }
